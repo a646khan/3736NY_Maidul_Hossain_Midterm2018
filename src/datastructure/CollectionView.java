34e71995836ad1e32ef7a9d865cd364121c9b246
maidulgit <maidul360@gmail.com>
2018-09-12 05:11:03 -0400

    CollectionVIew, UseMap, UseQueue parts are done

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -17,7 +17,9 @@ public static void main(String[] args) {
         map.put(5, "San Jose");
         map.put(5, "Seattle");
 
+        System.out.println("Collection View of Values: ");
 
+        for(HashMap.Entry < Integer, String > values : map.entrySet())
+                System.out.println(values.getKey() +", " +values.getValue());
     }
-
 }
